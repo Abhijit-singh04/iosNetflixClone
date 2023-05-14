36f48ff51191cce2f26db6a4288d7fcd3b86d70c
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-05-15 01:48:31 +0530

    first commit

---
+++ Netflix clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix clone
+//
+//  Created by Abhijit singh on 16/04/23.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
