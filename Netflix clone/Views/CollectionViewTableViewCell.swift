d5923239d79d430dda51948951d728cb21d29eec
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-05-15 01:48:49 +0530

    first commit

--- Netflix clone/Views/CollectionViewTableViewCell.swift
+++ Netflix clone/Views/CollectionViewTableViewCell.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            
+        contentView.backgroundColor = .systemRed
+        }
+    
+    required init?(coder: NSCoder) {
+            fatalError()
+        }
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+   
 
 }
